454d30c01176e7cdcab9653ac02de2d83e38a046
福田 翔 <fukuty36@gmail.com>
2017-10-24 16:21:05 +0900

    タグ一覧表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,21 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-    @dup_tags = Tag.pluck(:title)
-
-    @tags = Tag.uniq.pluck(:title)
-
-    @tags_list = []
-
-    for i in @tags do
-      count = 0
-        for j in @dup_tags do
-          if i == j
-              count += 1
-          @tags_list << count
-         end
-       end
-    end
+    @tags = Tag.group(:title).count
   end
 
   def show
