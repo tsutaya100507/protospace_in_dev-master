a78796c7a2dcda945b4365c03b59293915ff209c
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-24 10:06:05 +0900

    Edit access_key_id & secret_accsess_key
    IAMユーザーを作成後、アクセスキーIDとアクセスキーを記述

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAJ6Z44IIIOOVHCQ6A',
-    aws_secret_access_key: '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',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'proto-space-photo'
-  config.cache_storage = :fog
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/proto-space-photo'
 end
