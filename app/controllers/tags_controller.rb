3ff97427ff6ef518b9ed3da612631d9f83e0c378
福田 翔 <fukuty36@gmail.com>
2017-10-25 09:09:22 +0900

    タグ付け機能　タグ一覧　viewを整えた。

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,25 @@
 class TagsController < ApplicationController
+before_action :group_tags, only: [:index, :show]
+before_action :set_prototype, only: [:index, :show]
 
   def index
-    @tags = Tag.group(:title).count
+    @prototypes = Prototype.all.page(params[:page]).per(5)
   end
 
   def show
+    @tag = Tag.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
     @tag = Tag.new(tag_params)
+  end
+
+private
+  def group_tags
+    @tags = Tag.group(:title).count
+  end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
---
+++ db/migrate/20171024121223_add_user.rb
@@ -0,0 +1,4 @@
+class AddUser < ActiveRecord::Migration
+  def change
+  end
+end
