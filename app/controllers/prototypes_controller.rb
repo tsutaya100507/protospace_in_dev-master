b0945e75b188a6ec5ca2f746735f04613d24fc1f
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-26 20:04:15 +0900

    Resolve conflict
    前回マージ前に解決し損ねたと思しきコンフリクトを解消。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,30 +8,15 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
     @prototype.captured_images.build
-<<<<<<< HEAD
-    @sub = @prototype.captured_images.where(status: 1)
-
-=======
-
     @prototype.tags.build
-
-    # @sub = @prototype.captured_images.where(status: 1).first
->>>>>>> master
-
   end
 
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-<<<<<<< HEAD
       redirect_to :root, notice: 'New prototype was successfully created'
     else
       redirect_to ({ action: new }), alert: 'New prototype was unsuccessfully created'
-=======
-        redirect_to :root, notice: 'New prototype was successfully created'
-      else
-        redirect_to :root, alert: 'YNew prototype was unsuccessfully created'
->>>>>>> master
     end
   end
 
@@ -47,11 +32,7 @@ class PrototypesController < ApplicationController
   end
 
   def update
-<<<<<<< HEAD
     @prototype.update(update_prototype_params)
-=======
-    @prototype.update(prototype_params)
->>>>>>> master
     redirect_to :root, notice: 'Prototype was successfully updated'
   end
 
@@ -75,20 +56,12 @@ class PrototypesController < ApplicationController
       :catch_copy,
       :concept,
       :user_id,
-<<<<<<< HEAD
-      captured_images_attributes: [:content, :status]
-=======
-
       { :tag_ids => [] },
       captured_images_attributes: [:id, :content, :status],
       tags_attributes: [:title]
       )
-
-      captured_images_attributes: [:id, :content, :status, :prototype_id]
->>>>>>> master
-    )
-
   end
+
   def update_prototype_params
   params.require(:prototype).permit(
       :title,
@@ -97,5 +70,5 @@ class PrototypesController < ApplicationController
       :user_id,
       captured_images_attributes: [:id, :content, :status, :prototype_id, :content_cache]
     )
-end
+ end
 end
