9a244fb97977938c2616894c00374df3f6c3c7d7
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-28 14:48:07 +0900

    Edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,12 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     # region: 'ap-northeast-1'
-    aws_access_key_id: ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_KEY'],
-    region: ENV['S3_REGION']
+    # aws_access_key_id: ENV['S3_ACCESS_KEY'],
+    # aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    # region: ENV['S3_REGION']
   }
 
 case Rails.env
